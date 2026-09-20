b50d589e0828cef4a9c28546a6460ee960785d30
jshncd01-source <jshncd01@gmail.com>
2026-09-21 02:27:07 +0900

    chore(calendar): advance continuation asset generation

--- site-calendar-actions.js
+++ site-calendar-actions.js
@@ -1,6 +1,6 @@
-import {createLifeActivity} from './site-calendar.js?v=20260921-convcal2';
-import {createGuestCalendarRepository} from './site-calendar-guest.js?v=20260921-convcal2';
-import {getCurrentSiteUser, SiteCoreError} from './site-core.js?v=20260921-convcal2';
+import {createLifeActivity} from './site-calendar.js?v=20260921-convcal3';
+import {createGuestCalendarRepository} from './site-calendar-guest.js?v=20260921-convcal3';
+import {getCurrentSiteUser, SiteCoreError} from './site-core.js?v=20260921-convcal3';
 
 const ACTION_ID_RE = /^calact_[0-9a-f]{24}$/;
 const CANDIDATE_ID_RE = /^calcand_[0-9a-f]{24}$/;
