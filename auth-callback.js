40d8fb05e320758e616d9233d9cc43863402981c
jshncd01-source <jshncd01@gmail.com>
2026-09-20 11:30:35 +0900

    PHASE 10: pin conversation management assets

--- auth-callback.js
+++ auth-callback.js
@@ -5,7 +5,7 @@ import {
   SiteHandoffClientError,
 } from './site-auth.js?v=20260920-fallback4';
 import {redeemSiteHandoff, SiteCoreError} from './site-core.js?v=20260920-guest3';
-import {mountConversation} from './site-conversation.js?v=20260920-calnav9';
+import {mountConversation} from './site-conversation.js?v=20260920-conv10';
 import {mountLifeCalendarIfEnabled} from './site-calendar-ui.js?v=20260920-calnav9';
 
 const callbackShell = document.getElementById('auth-callback-shell');
