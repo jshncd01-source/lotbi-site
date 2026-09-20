135190e41145a734ec0e89a3b727303ca1fd7f0a
jshncd01-source <jshncd01@gmail.com>
2026-09-20 23:30:46 +0900

    chore(calendar): bust manager runtime cache

--- site-calendar-ui.js
+++ site-calendar-ui.js
@@ -1,6 +1,6 @@
 import {getLifeAgenda, getLifeAttention, getLifeToday, getLifeUpcoming, removeLifeActivity, rescheduleLifeActivity} from './site-calendar.js';
 import {SiteCoreError} from './site-core.js';
-export {buildCalendarAriaLabel, countCalendarEventsByMonth, loadLifeCalendarManagerView, mountLifeCalendarManager} from './site-calendar-manager.js?v=20260920-calux1';
+export {buildCalendarAriaLabel, countCalendarEventsByMonth, loadLifeCalendarManagerView, mountLifeCalendarManager} from './site-calendar-manager.js?v=20260920-calfocus1';
 
 const SESSION_STATE_EVENT = 'lotbi:site-session-state';
 const DEFAULT_TIMEZONE = 'Asia/Seoul';
