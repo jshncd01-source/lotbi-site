8b3c589a803168fb9c7fe4177db4ac78e0f185c8
jshncd01-source <jshncd01@gmail.com>
2026-09-20 22:20:19 +0900

    chore(calendar): publish Conversation Calendar assets

--- auth-callback.js
+++ auth-callback.js
@@ -6,9 +6,9 @@ import {
   recoverMissingSiteHandoffContext,
   SiteHandoffClientError,
 } from './site-auth.js?v=20260920-authux1';
-import {redeemSiteHandoff, SiteCoreError} from './site-core.js?v=20260920-guest3';
-import {mountConversation} from './site-conversation.js?v=20260920-calux1';
-import {mountLifeCalendarIfEnabled} from './site-calendar-ui.js?v=20260920-calux1';
+import {redeemSiteHandoff, SiteCoreError} from './site-core.js?v=20260920-convcal1';
+import {mountConversation} from './site-conversation.js?v=20260920-convcal1';
+import {mountLifeCalendarIfEnabled} from './site-calendar-ui.js?v=20260920-convcal1';
 
 const callbackShell = document.getElementById('auth-callback-shell');
 const titleNode = document.getElementById('auth-callback-title');
