1acd0680c94464ecc866e04fb3f75639a3c318e6
jshncd01-source <jshncd01@gmail.com>
2026-09-20 14:18:54 +0900

    merge: preserve latest main auth-start.js

--- auth-start.js
+++ auth-start.js
@@ -1,4 +1,4 @@
-import {beginSiteHandoff, clearSiteLogoutSuppression} from './site-auth.js?v=20260920-fallback4';
+import {beginSiteHandoff, clearSiteLogoutSuppression} from './site-auth.js?v=20260920-authux1';
 
 const errorShell = document.getElementById('auth-start-error-shell');
 const statusNode = document.getElementById('auth-start-status');
