e6fa3b4a90ad7a8ba38f23e71776eb87f77fc5ba
jshncd01-source <jshncd01@gmail.com>
2026-09-20 13:44:25 +0900

    fix: bottom-align active conversation composer

--- site-conversation.js
+++ site-conversation.js
@@ -39,7 +39,7 @@ function ensureConversationStyles() {
   if (document.querySelector('link[data-site-conversation-styles]')) return;
   const link = document.createElement('link');
   link.rel = 'stylesheet';
-  link.href = '/site-conversation.css?v=20260920-attachments2';
+  link.href = '/site-conversation.css?v=20260920-attachments3';
   link.dataset.siteConversationStyles = 'true';
   document.head.appendChild(link);
 }
