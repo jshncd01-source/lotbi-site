368a7308932af7c8f9d40df6fd090115a3b1101f
jshncd01-source <jshncd01@gmail.com>
2026-09-22 16:16:55 +0900

    Calendar: align holiday cache keys across auth/home

--- auth-callback.js
+++ auth-callback.js
@@ -7,8 +7,8 @@ import {
   SiteHandoffClientError,
 } from './site-auth.js?v=20260920-authux1';
 import {redeemSiteHandoff, SiteCoreError} from './site-core.js?v=20260921-guestclaim1';
-import {mountConversation} from './site-conversation.js?v=20260922-deleteconfirm1';
-import {mountLifeCalendarIfEnabled} from './site-calendar-ui.js?v=20260922-deleteconfirm1';
+import {mountConversation} from './site-conversation.js?v=20260922-holiday1';
+import {mountLifeCalendarIfEnabled} from './site-calendar-ui.js?v=20260922-holiday1';
 import {claimGuestConversationToAccount} from './site-conversation-storage.js?v=20260921-guestclaim1';
 
 const callbackShell = document.getElementById('auth-callback-shell');
