9d2b11882f2dce8915c878d6cbdd465757bc4ba4
jshncd01-source <jshncd01@gmail.com>
2026-09-22 19:30:41 +0900

    chore(site): align auth callback reminder runtime

--- auth-callback.js
+++ auth-callback.js
@@ -7,7 +7,7 @@ import {
   SiteHandoffClientError,
 } from './site-auth.js?v=20260920-authux1';
 import {redeemSiteHandoff, SiteCoreError} from './site-core.js?v=20260921-guestclaim1';
-import {mountConversation} from './site-conversation.js?v=20260922-permissionsettings1';
+import {mountConversation} from './site-conversation.js?v=20260922-reminderbell2';
 import {claimGuestConversationToAccount} from './site-conversation-storage.js?v=20260921-guestclaim1';
 
 const callbackShell = document.getElementById('auth-callback-shell');
