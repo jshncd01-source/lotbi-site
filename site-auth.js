5444f58ddd38acc7ec8ba9c24e8f6b76f6764ed0
jshncd01-source <jshncd01@gmail.com>
2026-09-24 21:05:13 +0900

    build(cache): normalize nested runtime asset URLs

--- site-auth.js
+++ site-auth.js
@@ -1,4 +1,4 @@
-import {SITE_CALLBACK_URI} from './site-core.js?v=20260924-assurance1';
+import {SITE_CALLBACK_URI} from './site-core.js?v=aset-pending';
 
 export const ACCOUNT_SITE_HANDOFF_URL = 'https://account.lotbiai.com/auth/site-handoff';
 export const ACCOUNT_SITE_FALLBACK_URL = 'https://account.lotbiai.com/?site_fallback=1';
