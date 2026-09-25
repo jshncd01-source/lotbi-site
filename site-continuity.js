4ebaf4fb25bc0f5769f14cd0b725ed30e6c08741
jshncd01-source <jshncd01@gmail.com>
2026-09-26 01:36:33 +0900

    chore(site): refresh deterministic asset cache token

--- site-continuity.js
+++ site-continuity.js
@@ -1,5 +1,5 @@
-import {beginSiteHandoff, clearSiteLogoutSuppression, hasSiteLogoutSuppression, markSiteLogoutSuppression, readAccountSessionStatus} from './site-auth.js?v=aset-e5c6cf672ce0';
-import {prepareGuestConversationClaimIntent} from './site-conversation-storage.js?v=aset-e5c6cf672ce0';
+import {beginSiteHandoff, clearSiteLogoutSuppression, hasSiteLogoutSuppression, markSiteLogoutSuppression, readAccountSessionStatus} from './site-auth.js?v=aset-a71c73117254';
+import {prepareGuestConversationClaimIntent} from './site-conversation-storage.js?v=aset-a71c73117254';
 
 export const SITE_SESSION_STATE_EVENT = 'lotbi:site-session-state';
 export const AUTH_STATE_CHECKING = 'checking';
