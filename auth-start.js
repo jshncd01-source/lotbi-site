d8de211a01485d5529840c898d116a78073852b1
jshncd01-source <jshncd01@gmail.com>
2026-09-25 17:39:56 +0900

    Regenerate asset cache refs (auth-start.js)

--- auth-start.js
+++ auth-start.js
@@ -1,4 +1,4 @@
-import {beginSiteHandoff, clearSiteLogoutSuppression} from './site-auth.js?v=aset-6381989b8794';
+import {beginSiteHandoff, clearSiteLogoutSuppression} from './site-auth.js?v=aset-55317470c4fa';
 
 const errorShell = document.getElementById('auth-start-error-shell');
 const statusNode = document.getElementById('auth-start-status');
