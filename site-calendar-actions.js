2fac0d6ab6645e8cfee39a6a8a64d7d02d26b601
jshncd01-source <jshncd01@gmail.com>
2026-09-26 01:35:32 +0900

    chore(site): refresh deterministic asset cache token

--- site-calendar-actions.js
+++ site-calendar-actions.js
@@ -1,6 +1,6 @@
-import {createLifeActivity} from './site-calendar.js?v=aset-e5c6cf672ce0';
-import {createGuestCalendarRepository} from './site-calendar-guest.js?v=aset-e5c6cf672ce0';
-import {getCurrentSiteUser, SiteCoreError} from './site-core.js?v=aset-e5c6cf672ce0';
+import {createLifeActivity} from './site-calendar.js?v=aset-a71c73117254';
+import {createGuestCalendarRepository} from './site-calendar-guest.js?v=aset-a71c73117254';
+import {getCurrentSiteUser, SiteCoreError} from './site-core.js?v=aset-a71c73117254';
 
 const ACTION_ID_RE = /^calact_[0-9a-f]{24}$/;
 const CANDIDATE_ID_RE = /^calcand_[0-9a-f]{24}$/;
