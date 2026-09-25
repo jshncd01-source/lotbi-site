a37c2304bd2710ce301171f96c2f31e073ed6e11
jshncd01-source <jshncd01@gmail.com>
2026-09-25 11:20:23 +0900

    feat: clarify home value and navigation

--- site-calendar-actions.js
+++ site-calendar-actions.js
@@ -1,6 +1,6 @@
-import {createLifeActivity} from './site-calendar.js?v=aset-ab33a97493b7';
-import {createGuestCalendarRepository} from './site-calendar-guest.js?v=aset-ab33a97493b7';
-import {getCurrentSiteUser, SiteCoreError} from './site-core.js?v=aset-ab33a97493b7';
+import {createLifeActivity} from './site-calendar.js?v=aset-cd9d674a8149';
+import {createGuestCalendarRepository} from './site-calendar-guest.js?v=aset-cd9d674a8149';
+import {getCurrentSiteUser, SiteCoreError} from './site-core.js?v=aset-cd9d674a8149';
 
 const ACTION_ID_RE = /^calact_[0-9a-f]{24}$/;
 const CANDIDATE_ID_RE = /^calcand_[0-9a-f]{24}$/;
